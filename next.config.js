6fa1f669e134b970144772ccc46299e4d376c187
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-04-03 18:24:36 +0900

    split

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const { withSplit } = require('next-with-split')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  splits: {
+    'challenger-1': {
+      path: '/',
+      hosts: {
+        original: 'split-example.vercel.app',
+        'challenger-1': 'split-example-git-challenger-1-survaq-team.vercel.app'
+      }
+    }
+  },
 }
 
 module.exports = withSplit(nextConfig)
