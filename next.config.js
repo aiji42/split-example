6206b3c519d05e60e90da32178cac7bfb8fe9825
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-04-04 11:54:15 +0900

    add challnger-1

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-const withSplit = require('next-with-split').withSplit({
-  splits: {
-    'test1': {
-      path: '/',
-      hosts: {
-        original: 'split-example.vercel.app',
-        'challenger-1': 'split-example-git-challenger-1-survaq-team.vercel.app'
-      }
-    }
-  }
-})
+const withSplit = require('next-with-split').withSplit({})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
